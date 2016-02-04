6ddeb0f541a99516b891ed27e7f99ac42caa8ad4
Nick Brennan <nick_p_b@yahoo.com>
2016-02-04 14:46:24 -0800

    user schema created and passport setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //express config
 var express = require('express');
 var app = express();
-var session = require('express-sesion');
+var session = require('express-session');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
@@ -11,7 +11,8 @@ var crypto = require('crypto');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var views = path.join(process.cwd(), "views"); 
+var views = path.join(process.cwd(), "views");
+var db = require('./models'); 
 
 //middleware config
 app.set('port', process.env.PORT || 3000);
@@ -21,12 +22,51 @@ app.use('/vendor', express.static('bower_components'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.use(session({secret: 'mind boggling session key'}));
+
+//passport setup
+passport.use(new LocalStrategy(function(username, password, done){
+	User.findOne({username: username}, function(err, user){
+		if(err) return done(err);
+		if(!user) return done(null, false, {message: "Wrong Username"});
+
+		user.comparePassword(password, function(err, isMatch){
+			if(isMatch){
+				return done(null, user);
+			}else{
+				return done(null, false, {message: "Wrong Password"});
+			}
+		});
+	});
+}));
+
+passport.serializeUser(function(user, done){
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+	User.findByID(id, function(err, user){
+		done(err, user);
+	});
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routes
 app.get('/', function(req, res){
-	res.sendFile(views + "/home.html");
+	res.render('index', {
+		title: 'Express',
+		user: req.user
+	});
 });
 
+app.get('/login', function(req, res){
+	res.render('login', {
+		user: req.user
+	})
+});
+//initialize server
 app.listen(app.get('port'), function(){
 	console.log('Crush is Keyaling on port' + app.get('port'));
 });
--- models/index.js
+++ models/index.js
@@ -2,3 +2,5 @@ var mongoose = require('mongoose');
 mongoose.connect(	process.env.MONGOLAB_URI ||
 					process.env.MONGOHQ_URL ||
 					"mongodb://localhost/crushIt");
+
+module.exports.User = require('./user.js');
